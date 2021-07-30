98eba32aac61d0e965c986eb309bdbd10514492a
Gwyn Bong <gwynbong@gmail.com>
2021-07-30 18:38:35 +0800

    update posting ui

--- lib/views/posting/add_post_view.dart
+++ lib/views/posting/add_post_view.dart
@@ -4,9 +4,13 @@
  * Last modified 16/7/21 11:42 PM
  */
 
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:nearbyou/models/route_data_model.dart';
 import 'package:nearbyou/models/suggestions_model.dart';
+import 'package:nearbyou/utilities/constants/constants.dart';
 import 'package:nearbyou/utilities/ui/components/rounded_icon_button.dart';
 import 'package:nearbyou/utilities/ui/palette.dart';
 import 'package:nearbyou/views/posting/add_route_view.dart';
@@ -27,23 +31,25 @@ class AddPostView extends StatefulWidget {
 }
 
 class _AddPostViewState extends State<AddPostView> {
-  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
   TextEditingController _startPointCon = TextEditingController();
   TextEditingController _endPointCon = TextEditingController();
   TextEditingController _postDescCon = TextEditingController();
 
-  TextEditingController _locationNameCon = TextEditingController();
-  TextEditingController _captionCon = TextEditingController();
-
   final startPointFocus = FocusNode();
   final endPointFocus = FocusNode();
   final postDescFocus = FocusNode();
-  final _locationNameFocus = FocusNode();
-  final _captionFocus = FocusNode();
 
-  List<RouteData> routeData = [];
-  List<String> _items = [];
+  GoogleMapController googleMapController;
+  LatLng _lastMapPosition = initialPosition;
+  Set<Marker> _markers = HashSet<Marker>();
+
+  void _onMapCreated(GoogleMapController controller) {
+    googleMapController = controller;
+  }
+
+  void _onCameraMove(CameraPosition position) {
+    _lastMapPosition = position.target;
+  }
 
   Future<String> searchPlaces() async {
     final sessionToken = Uuid().v4();
@@ -57,103 +63,6 @@ class _AddPostViewState extends State<AddPostView> {
     return result.placeDesc;
   }
 
-  void reorderRouteData(int oldIndex, int newIndex) {
-    setState(() {
-      if (newIndex > oldIndex) {
-        newIndex -= 1;
-      }
-      final items = _items.removeAt(oldIndex);
-      _items.insert(newIndex, items);
-    });
-  }
-
-  _buildCard(String items, int index) {
-    return Card(
-      color: Colors.white,
-      key: ValueKey(index),
-      elevation: 2,
-      child: Column(
-        children: [
-          ExpansionTile(
-            title: Text('Location name'),
-            trailing: ReorderableDragStartListener(
-              index: 0,
-              child: Icon(Icons.drag_handle),
-            ),
-            children: [
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 25),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      TextFormField(
-                          controller: _locationNameCon,
-                          focusNode: _locationNameFocus,
-                          decoration: InputDecoration(
-                            hintText: 'Location name',
-                          ),
-                          validator: (value) => value.isEmpty
-                              ? 'Location name cannot be blank'
-                              : null),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      TextFormField(
-                          controller: _captionCon,
-                          focusNode: _captionFocus,
-                          decoration: InputDecoration(
-                            hintText: 'Caption',
-                          ),
-                          validator: (value) =>
-                              value.isEmpty ? 'Caption cannot be blank' : null),
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              // Container(
-              //   alignment: Alignment.center,
-              //   child: Image(
-              //       image: AssetImage('assets/images/default-profile.png')),
-              // ),
-            ],
-          ),
-          ButtonTheme(
-            child: ButtonBar(
-              alignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.edit),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(
-                    Icons.delete,
-                    color: Colors.redAccent,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      _items.removeAt(index);
-                    });
-                  },
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  _addRouteCard() {
-    setState(() {
-      _items.add('hello');
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -165,6 +74,7 @@ class _AddPostViewState extends State<AddPostView> {
       child: Scaffold(
         resizeToAvoidBottomInset: false,
         appBar: AppBar(
+          //Before closing, prompt user if want to save as draft or discard post
           leading: CloseButton(
             onPressed: () => Navigator.of(context).pop(),
             color: Colors.black,
@@ -174,6 +84,7 @@ class _AddPostViewState extends State<AddPostView> {
             style: TextStyle(color: Colors.black),
           ),
           actions: [
+            //this should save to firestore
             IconButton(
                 icon: Icon(Icons.check, color: Colors.black), onPressed: () {}),
           ],
@@ -182,98 +93,105 @@ class _AddPostViewState extends State<AddPostView> {
         ),
         floatingActionButton: RoundedIconButton(
           icon: Icons.add,
-          onPressed: _addRouteCard,
+          onPressed: () {},
         ),
         // SpeedDialWidget(),
-        body: Column(
-          children: [
-            Expanded(
-              flex: 3,
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Padding(
-                  padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      SearchFieldContainer(
-                        controller: _startPointCon,
-                        focusNode: startPointFocus,
-                        labelText: "Start",
-                        hintText: "Choose starting point",
-                        prefixIcon: Icon(
-                          Icons.location_on_rounded,
-                          color: textLightColor,
-                        ),
-                        suffixIcon: IconButton(
-                          icon: Icon(Icons.my_location),
-                          color: primaryColor,
-                          onPressed: () {},
-                        ),
-                        onTap: () async {
-                          final place = await searchPlaces();
-                          setState(() {
-                            _startPointCon.text = place;
-                          });
-                        },
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      SearchFieldContainer(
-                        controller: _endPointCon,
-                        focusNode: endPointFocus,
-                        labelText: "Destination",
-                        hintText: "Choose destination point",
-                        prefixIcon: Icon(
-                          Icons.flag,
-                          color: textLightColor,
-                        ),
-                        onTap: () async {
-                          final place = await searchPlaces();
-                          setState(() {
-                            _endPointCon.text = place;
-                          });
-                        },
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      buildDivider(),
-                      Container(
-                        margin: EdgeInsets.symmetric(horizontal: 30),
-                        child: SingleChildScrollView(
-                          child: TextFormField(
-                            focusNode: postDescFocus,
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: 'Write something here ....',
-                              hintMaxLines: 4,
-                            ),
-                            controller: _postDescCon,
-                            keyboardType: TextInputType.multiline,
-                            maxLines: 5,
-                          ),
-                        ),
-                      ),
-                    ],
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.all(15),
+            height: MediaQuery.of(context).size.height,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SearchFieldContainer(
+                    controller: _startPointCon,
+                    focusNode: startPointFocus,
+                    labelText: "Start",
+                    hintText: "Choose starting point",
+                    prefixIcon: Icon(
+                      Icons.location_on_rounded,
+                      color: textLightColor,
+                    ),
+                    suffixIcon: IconButton(
+                      icon: Icon(Icons.my_location),
+                      color: primaryColor,
+                      onPressed: () {},
+                    ),
+                    onTap: () async {
+                      final place = await searchPlaces();
+                      setState(() {
+                        _startPointCon.text = place;
+                      });
+                    },
                   ),
-                ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  SearchFieldContainer(
+                    controller: _endPointCon,
+                    focusNode: endPointFocus,
+                    labelText: "Destination",
+                    hintText: "Choose destination point",
+                    prefixIcon: Icon(
+                      Icons.flag,
+                      color: textLightColor,
+                    ),
+                    onTap: () async {
+                      final place = await searchPlaces();
+                      setState(() {
+                        _endPointCon.text = place;
+                      });
+                    },
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  buildDivider(),
+                  Container(
+                    margin: EdgeInsets.only(left: 30, right: 30),
+                    child: TextFormField(
+                      focusNode: postDescFocus,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Write something here ....',
+                        hintMaxLines: 4,
+                      ),
+                      controller: _postDescCon,
+                      keyboardType: TextInputType.multiline,
+                      maxLines: 3,
+                    ),
+                  ),
+                  buildDivider(),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 500,
+                    child: buildGoogleMap(),
+                  ),
+                ],
               ),
             ),
-            buildDivider(),
-            Expanded(
-              flex: 3,
-              child: ReorderableListView.builder(
-                  itemBuilder: (context, index) {
-                    return _buildCard(_items[index], index);
-                  },
-                  itemCount: _items.length,
-                  onReorder: reorderRouteData),
-            ),
-          ],
+          ),
         ),
       ),
+    );
+  }
+
+  GoogleMap buildGoogleMap() {
+    return GoogleMap(
+      onMapCreated: _onMapCreated,
+      myLocationButtonEnabled: false,
+      myLocationEnabled: false,
+      zoomGesturesEnabled: true,
+      zoomControlsEnabled: false,
+      mapToolbarEnabled: false,
+      initialCameraPosition: CameraPosition(
+        target: initialPosition,
+        zoom: 11.0,
+      ),
+      mapType: MapType.normal,
+      markers: _markers,
+      onCameraMove: _onCameraMove,
+      // onTap: _handleTap,
     );
   }
 
@@ -282,6 +200,7 @@ class _AddPostViewState extends State<AddPostView> {
     // TODO: implement dispose
     _startPointCon.dispose();
     _endPointCon.dispose();
+    _postDescCon.dispose();
     super.dispose();
   }
 }
